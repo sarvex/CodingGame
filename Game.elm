c86261333700d25cc29e173935499107077518fa
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-06-27 19:51:34 +0400

    Animated gifs added

--- Game.elm
+++ Game.elm
@@ -176,7 +176,8 @@ record_to_action rec =
      | otherwise -> None
 
 
-main = lift (\json->asText (record_to_action json)) code_port
+--main = lift (\json->asText (record_to_action json)) code_port
+main = lift (\s-> collage 500 500 [move(toFloat (round(inMilliseconds (s/5))), 10)(toForm (image 57 49 "imgs/man/man_walks_right.gif"))]) (foldp (+) 0 (fps 50))
 
 -- This function is exported to python as _game.summarize (see its usages in game.py)
 port summarize: Int -> Int -> Int
