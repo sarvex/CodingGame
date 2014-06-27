cba18ac365a9dc0b58cac87231bb4db0c834fc2e
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-06-27 21:32:39 +0200

    Use less code

--- initial_code.py
+++ initial_code.py
@@ -1,12 +1,7 @@
 import game
 
-
 def hero_action(game_info):
-    """
-
-    :type game_info game.GameInfo
-    """
-    if game_info.obstacle_front:
+   if game_info.obstacle_front:
         return game.Jump()
     else:
         return game.Forward()
