06b47300a8a559ec370f547e1ac606257b16c7da
Dmitry Trofimov <dmitry.trofimov@jetbrains.com>
2014-06-28 03:26:56 +0200

    Updated initial python.

--- initial_code.py
+++ initial_code.py
@@ -4,7 +4,7 @@ from game_api import *
 def robot_action(robot):
     # print robot.gps.x
 
-    if robot.sensors.is_barrier_near() or robot.gps.x > 446:
+    if robot.sensors.is_barrier_near() or robot.sensors.pick_material(1, -1) == 'WATER':
         return Jump()
     else:
         return Forward()
